028147d985d4efd3485e5d9cc6b6f66680721a1e
KARA PEDERSEN <KJPED74@GMAIL.COM>
2021-04-05 17:08:05 -0700

    set up function

---
+++ utils.js
@@ -0,0 +1,16 @@
+import { pokemon } from './pokemon.js';
+
+export function findById(array, id) {
+    for (let item of array) {
+        if (item.name === id) {
+            return item;
+        }
+    }
+}
+
+export function randomPoke(array) {
+
+    const pokeNumber = Math.floor(Math.random() * array.length);
+
+    return array[pokeNumber];
+}
