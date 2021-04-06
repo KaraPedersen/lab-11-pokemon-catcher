c4775bd52c3ccc8dabdf769f827f82ca674d7bc6
KARA PEDERSEN <KJPED74@GMAIL.COM>
2021-04-05 20:06:41 -0700

    broke it

---
+++ local-storage-utils.js
@@ -0,0 +1,51 @@
+import { findById } from './utils.js';
+
+const POKEDEX = 'POKEDEX';
+
+export function getPokedex() {
+    const stringyPokedex = localStorage.getItem(POKEDEX);
+
+    if (!stringyPokedex) return [];
+
+    const parsedPokedex = JSON.parse(stringyPokedex);
+
+    return parsedPokedex;
+}
+
+export function setPokedex(parsedPokedex) {
+    const stringyPokedex = JSON.stringy(parsedPokedex);
+
+    localStorage.setItem(POKEDEX, stringyPokedex);
+}
+
+export function encounterPokemon(pokemon) {
+
+    const pokedex = getPokedex();
+    const matchingPokedexItem = findById(pokedex, pokemon.pokemon);
+
+    if (matchingPokedexItem) {
+        matchingPokedexItem.encountered++;
+    } else {
+        const newPokedexItem = {
+            id: pokemon.pokemon,
+            captured: 0,
+            encountered: 1
+        };
+        pokedex.push(newPokedexItem);
+    }
+
+    setPokedex(pokedex);
+
+    return pokedex;
+}
+
+export function capturePokemon(pokemon) {
+
+    const pokedex = getPokedex();
+
+    const matchingPokedexItem.captured++
+
+        setPokedex(pokedex);
+
+    return pokedex;
+}
